68831c2ce24aa894d3e6bf95cde26a810a313d3c
jobsons10 <jobsonsobrinho@grupootg.com>
2023-02-24 14:31:00 -0400

    final

--- presenteestrella-bonus2/app.js
+++ presenteestrella-bonus2/app.js
@@ -2,42 +2,27 @@ const puppeteer = require("puppeteer");
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
-  console.log("1");
   const page = await browser.newPage();
-  console.log("2");
   await page.setViewport({
     width: 1920,
     height: 1080,
-    deviceScaleFactor: 1,
+    deviceScaleFactor: 1, 
   });
   
-  await page.goto("http://presentedocacique.com/aviator");
-  await page.click("#button");
-  await new Promise(r => setTimeout(r, 16000));
+  await page.goto("https://www.presentedocacique.com/bonus2/");
 
-  // type fullname
-  const fullname = '#fullname';
-  await page.waitForSelector(fullname);
-  await page.type(fullname, 'Jobson teste');
+  // click first button
+  await page.click("#spin-bonus2");
+  await new Promise(r => setTimeout(r, 8000));
 
-  // type email
-  const email = '#email';
-  await page.waitForSelector(email);
-  await page.type(email, 'jobsonbritsao10@gmail.com');
+  // click second button
+  const takeBonus2 = '#take-bonus2';
+  await page.waitForSelector(takeBonus2);
+  await page.click(takeBonus2)
 
-  // type phone
-  const phone = '#phone';
-  await page.waitForSelector(phone);
-  await page.type(phone, 'jobsonbritsao10@gmail.com');
-
-  // submit form
-  const submitBtn = '#_form_14_submit';
-  await page.waitForSelector(submitBtn);
-  await page.click(submitBtn)
-
-  await new Promise(r => setTimeout(r, 4000));
-  await console.log(page.url());
   await new Promise(r => setTimeout(r, 7000));
+
+  // retornar url
   await console.log(page.url());
 
   // other actions...
